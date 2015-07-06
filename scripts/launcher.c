f37b91f8d59334a91ea735389f556afc623e2acf
Daniel Scharrer <daniel@constexpr.org>
2015-07-06 12:42:21 +0200

    Add support for creating launchers

--- scripts/launcher.c
+++ scripts/launcher.c
@@ -25,6 +25,8 @@
 #define CAT(x, y) CAT_HELPER(x, y)
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof(*(a)))
 
+#include <stdio.h>
+
 BOOL IsWow64() {
 	
 	typedef BOOL (WINAPI *LPFN_ISWOW64PROCESS) (HANDLE, PBOOL);
@@ -48,6 +50,34 @@ BOOL IsWow64() {
 	return bIsWow64;
 }
 
+void flush_slashes(LPTSTR * opp, unsigned * slash_countp) {
+	LPTSTR op = *opp;
+	unsigned slash_count = *slash_countp;
+	for(; slash_count; slash_count--) {
+		*op++ = '\\';
+		*op++ = '\\';
+	}
+	*opp = op;
+	*slash_countp = 0;
+}
+
+void append_cmdline(LPTSTR * opp, unsigned * slash_countp, LPCTSTR str) {
+	LPTSTR op = *opp;
+	unsigned slash_count = *slash_countp;
+	for(; *str; str++) {
+		switch(*str) {
+			case '\\': slash_count++; continue;
+			case '"': flush_slashes(&op, &slash_count);
+		}
+		for(; slash_count; slash_count--) {
+			*op++ = '\\';
+		}
+		*op++ = *str;
+	}
+	*opp = op;
+	*slash_countp = slash_count;
+}
+
 int CALLBACK WinMain(HINSTANCE a, HINSTANCE b, PSTR c, INT d) {
 	
 	// Save the console for our children
@@ -63,7 +93,7 @@ int CALLBACK WinMain(HINSTANCE a, HINSTANCE b, PSTR c, INT d) {
 	if(!GetModuleFileName(NULL, buffer, sizeof(buffer)) > 0) {
 		return 1;
 	}
-	LPTSTR p = _tcsrchr(buffer, '\\') + 1;
+	LPTSTR p = _tcsrchr(buffer, '\\');
 	
 	// Pass along the current executable directory
 	#if LAUNCHER_USE_CMD_PATH
@@ -71,12 +101,48 @@ int CALLBACK WinMain(HINSTANCE a, HINSTANCE b, PSTR c, INT d) {
 	SetEnvironmentVariable(STR(CAT(LAUNCHER_SCOMMAND, _PATH)), buffer);
 	#endif
 	
+	// Fudge the commandline
+	LPTSTR cmdline = GetCommandLine();
+	#ifdef LAUNCHER_ARGS
+	*p = 0;
+	TCHAR newcmdline[8191 * 2];
+	LPTSTR op = newcmdline;
+	unsigned slash_count = 0;
+	int n;
+	LPTSTR * args = CommandLineToArgvW(cmdline, &n);
+	TCHAR invocation[MAX_PATH];
+	_tcscpy(invocation, args[0]);
+	LPTSTR ip = _tcsrchr(invocation, '\\');
+	if(ip) {
+		ip++;
+	} else {
+		ip = invocation;
+	}
+	_tcscpy(invocation, STR(LAUNCHER_COMMAND));
+	#define LAUNCHER_ARG_BEGIN()    *op++ = ' '; *op++ = '"'
+	#define LAUNCHER_ARG_TEXT(Text) append_cmdline(&op, &slash_count, (Text));
+	#define LAUNCHER_ARG_HERE()     LAUNCHER_ARG_TEXT(buffer)
+	#define LAUNCHER_ARG_END()      flush_slashes(&op, &slash_count); *op++ = '"'
+	*op++ = '"';
+	LAUNCHER_ARG_TEXT(invocation);
+	LAUNCHER_ARG_END();
+	LAUNCHER_ARGS;
+	int i = 1;
+	for(; i < n; i++) {
+		LAUNCHER_ARG_BEGIN();
+		LAUNCHER_ARG_TEXT(args[i]);
+		LAUNCHER_ARG_END();
+	}
+	*op = 0;
+	cmdline = newcmdline;
+	#endif
+	
 	// Determine the subdirectory for the appropriate variant
 	LPCTSTR prefix;
 	if(IsWow64()) {
-		prefix = TEXT("bin\\x64\\");
+		prefix = TEXT("\\bin\\x64");
 	} else {
-		prefix = TEXT("bin\\x86\\");
+		prefix = TEXT("\\bin\\x86");
 	}
 	DWORD plen = _tcslen(prefix);
 	memcpy(p, prefix, plen * sizeof(TCHAR));
@@ -96,6 +162,7 @@ int CALLBACK WinMain(HINSTANCE a, HINSTANCE b, PSTR c, INT d) {
 	// Append the target command name
 	const TCHAR * const command = STR(LAUNCHER_COMMAND);
 	DWORD clen = _tcslen(command);
+	*p++ = '\\';
 	memcpy(p, command, clen * sizeof(TCHAR));
 	p += clen;
 	
@@ -108,7 +175,7 @@ int CALLBACK WinMain(HINSTANCE a, HINSTANCE b, PSTR c, INT d) {
 	PROCESS_INFORMATION pi;
 	ZeroMemory(&pi, sizeof(pi));
 	DWORD flags = CREATE_UNICODE_ENVIRONMENT;
-	if(!CreateProcess(buffer, GetCommandLine(), NULL, NULL, FALSE, flags, NULL, NULL, &si, &pi)) {
+	if(!CreateProcess(buffer, cmdline, NULL, NULL, FALSE, flags, NULL, NULL, &si, &pi)) {
 		return 1;
 	}
 	
