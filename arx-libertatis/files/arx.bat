1037f8c40403a76f1c16c122cf2055b50def3e4c
Daniel Scharrer <daniel@constexpr.org>
2020-09-09 20:46:27 +0200

    arx-libertatis: Hide console when launched from arx.bat

--- arx-libertatis/files/arx.bat
+++ arx-libertatis/files/arx.bat
@@ -1,4 +1,4 @@
 @echo off
 rem This file overwrites arx.bat shipped with the 1.22 update of Arx Fatalis
 rem in order to disable the __COMPAT_LAYER workaround which AL doesn't need.
-arx.exe
+arx.exe --hide-console %*
