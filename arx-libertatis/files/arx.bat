08c3870b45e169c8203acc6f9252d25f45c78e29
Daniel Scharrer <daniel@constexpr.org>
2021-06-28 22:47:00 +0200

    arx-libertatis: Use `arxtool hideconsole` instead of --hide-console

--- arx-libertatis/files/arx.bat
+++ arx-libertatis/files/arx.bat
@@ -1,4 +1,5 @@
 @echo off
 rem This file overwrites arx.bat shipped with the 1.22 update of Arx Fatalis
 rem in order to disable the __COMPAT_LAYER workaround which AL doesn't need.
-arx.exe --hide-console %*
+bin\x86\arxtool.exe hideconsole
+arx.exe %*
